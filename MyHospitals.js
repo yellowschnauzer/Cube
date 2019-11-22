bc752c48dabc12d983a0b40e965229cf9f036514
yellowschnauzer <yellowschnauzer@users.noreply.github.com>
2019-11-23 10:02:22 +1100

    Update MyHospitals.js

--- MyHospitals.js
+++ MyHospitals.js
@@ -1,85 +1,61 @@
-(function () {
-    var myConnector = tableau.makeConnector(); 
- 
- 
-	// Define the schema
-   myConnector.getSchema = function (schemaCallback) {
-    
-		var HospitalCols = [
-			{ id : "description", dataType : tableau.dataTypeEnum.string },
-//			{ id : "id", alias : "ID", dataType : tableau.dataTypeEnum.string },
-//			{ id : "isClosed", alias : "Closed", dataType : tableau.dataTypeEnum.boolean },
-//			{ id : "isPublic", alias : "Public", dataType : tableau.dataTypeEnum.boolean },
-//			{ id : "latitude", alias : "Latitude", dataType : tableau.dataTypeEnum.float },
-//			{ id : "longitude", alias : "Longitude", dataType : tableau.dataTypeEnum.float },
-			{ id : "name", dataType : tableau.dataTypeEnum.string },
-//			{ id : "phnCode", alias : "Code", dataType : tableau.dataTypeEnum.float },
-//			{ id : "phnName", alias : "Hospital", dataType : tableau.dataTypeEnum.string },
-			{ id : "state", dataType : tableau.dataTypeEnum.string }
-		];
+  
+(function() {
+    // Create the connector object
+    var myConnector = tableau.makeConnector();
 
-		var HospitalsTableInfo = {
-			id : "HospitalUrl",
-			alias : "Hospitals",
-			columns : HospitalCols
-		};
-		
-//		var IndicatorCols = [
-//			{ id : "code", alias : "Code", dataTypeEnum : tableau.dataTypeEnum.float },
-//			{ id : "indicatorName", alias : "Indicator", dataTypeEnum : tableau.dataTypeEnum.string },
-//			{ id : "name", alias : "Name", dataTypeEnum : tableau.dataTypeEnum.string },
-//			{ id : "quarterlyPeriods", alias : "Period", dataTypeEnum : tableau.dataTypeEnum.string },
-//			{ id : "unitOfMeasure", alias : "Measure", dataTypeEnum : tableau.dataTypeEnum.string }
-//		];
-		
-//		var IndicatorTableInfo = {
-//			id: "IndicatorUrl",
-//			alias : "Indicators",
-//			columns : IndicatorCols
-//		};
+    // Define the schema
+    myConnector.getSchema = function(schemaCallback) {
+        var cols = [{
+//            id: "id",
+//            dataType: tableau.dataTypeEnum.string
+//        }, {
+            id: "name",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "description",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "state",
+            dataType: tableau.dataTypeEnum.string
+        }];
 
+        var tableSchema = {
+            id: "earthquakeFeed",
+            alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
+            columns: cols
+        };
 
+        schemaCallback([tableSchema]);
+    };
 
-		schemaCallback([HospitalsTableInfo]);
+    // Download the data
+    myConnector.getData = function(table, doneCallback) {
+        $.getJSON("www.myhospitals.gov.au/api/datasets?indicatorId=111", function(resp) {
+            var feat = resp.hspitals,
+                tableData = [];
 
-	};
- 
- 
- // Download data
-    myConnector.getData = function (table, doneCallback) {
-		$.getJSON("www.myhospitals.gov.au/api/datasets?indicatorId=111", function (resp) {
-			var hospDataset = resp.hospitals,
-//				indiURL = resp.indicators,
-			    tableData = [];
-			
-			for (var i = 0, len = hospDataset.length; i < len; i++) {
-				tableData.push({
-					"description" : hospDataset[i].description,
-//					"id" : hospDataset[i].id,
-//					"isClosed" : hospDataset[i].isClosed,
-//					"isPublic" : hospDataset[i].isPublic,
-//					"latitude" : hospDataset[i].latitude,
-//					"longitude" : hospDataset[i].longitude,
-					"name" : hospDataset[i].name,
-//					"phnCode" : hospDataset[i].phnCode,
-//					"phnName" : hospDataset[i].phnName,
-					"state" : hospDataset[i].state
-				});
-			}
+            // Iterate over the JSON object
+            for (var i = 0, len = feat.length; i < len; i++) {
+                tableData.push({
+  //                  "id": feat[i].id,
+                    "name": feat[i].name,
+                    "description": feat[i].description,
+                    "state": feat[i].state
+                });
+            }
 
-			table.appendRows(tableData);
-
-			doneCallback();
-		});
-	};
+            table.appendRows(tableData);
+            doneCallback();
+        });
+    };
 
     tableau.registerConnector(myConnector);
-	
-	$(document).ready(function () {
-		$("#submitButton").click(function () {
-			tableau.connectionName = "MyHospitals";
-			tableau.submit();
-		});
-	});
-	
+
+    // Create event listeners for when the user submits the form
+    $(document).ready(function() {
+        $("#submitButton").click(function() {
+            tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
+            tableau.submit(); // This sends the connector object to Tableau
+        });
+    });
 })();
