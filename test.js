81e402710e403b02dcd9fdbfecd4704bdde1367b
yellowschnauzer <yellowschnauzer@users.noreply.github.com>
2019-11-23 00:32:37 +1100

    Update test.js

--- test.js
+++ test.js
@@ -4,8 +4,7 @@
 
     // Define the schema
     myConnector.getSchema = function(schemaCallback) {
-        // Schema for magnitude and place data
-        var mag_place_cols = [{
+        var cols = [{
             id: "id",
             dataType: tableau.dataTypeEnum.string
         }, {
@@ -17,75 +16,33 @@
             alias: "title",
             dataType: tableau.dataTypeEnum.string
         }, {
-            id: "lat",
-            alias: "latitude",
-            dataType: tableau.dataTypeEnum.float
-        }, {
-            id: "lon",
-            alias: "longitude",
-            dataType: tableau.dataTypeEnum.float
+            id: "location",
+            dataType: tableau.dataTypeEnum.geometry
         }];
 
-        var magPlaceTable = {
-            id: "magPlace",
-            alias: "Magnitude and Place Data",
-            columns: mag_place_cols
+        var tableSchema = {
+            id: "earthquakeFeed",
+            alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
+            columns: cols
         };
 
-        // Schema for time and URL data
-        var time_url_cols = [{
-            id: "id",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "time",
-            alias: "time",
-            dataType: tableau.dataTypeEnum.date
-        }, {
-            id: "url",
-            alias: "url",
-            dataType: tableau.dataTypeEnum.string
-        }];
-
-        var timeUrlTable = {
-            id: "timeUrl",
-            alias: "Time and URL Data",
-            columns: time_url_cols
-        };
-        schemaCallback([magPlaceTable, timeUrlTable]);
+        schemaCallback([tableSchema]);
     };
 
     // Download the data
     myConnector.getData = function(table, doneCallback) {
-        var dateObj = JSON.parse(tableau.connectionData),
-            dateString = "starttime=" + dateObj.startDate + "&endtime=" + dateObj.endDate,
-            apiCall = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&" + dateString + "&minmagnitude=4.5";
-
-        $.getJSON(apiCall, function(resp) {
+        $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
             var feat = resp.features,
                 tableData = [];
 
-            var i = 0;
-
-            if (table.tableInfo.id == "magPlace") {
-                for (i = 0, len = feat.length; i < len; i++) {
-                    tableData.push({
-                        "id": feat[i].id,
-                        "mag": feat[i].properties.mag,
-                        "title": feat[i].properties.title,
-                        "lon": feat[i].geometry.coordinates[0],
-                        "lat": feat[i].geometry.coordinates[1]
-                    });
-                }
-            }
-
-            if (table.tableInfo.id == "timeUrl") {
-                for (i = 0, len = feat.length; i < len; i++) {
-                    tableData.push({
-                        "id": feat[i].id,
-                        "url": feat[i].properties.url,
-                        "time": new Date(feat[i].properties.time) // Convert to a date format from epoch time
-                    });
-                }
+            // Iterate over the JSON object
+            for (var i = 0, len = feat.length; i < len; i++) {
+                tableData.push({
+                    "id": feat[i].id,
+                    "mag": feat[i].properties.mag,
+                    "title": feat[i].properties.title,
+                    "location": feat[i].geometry
+                });
             }
 
             table.appendRows(tableData);
@@ -98,24 +55,8 @@
     // Create event listeners for when the user submits the form
     $(document).ready(function() {
         $("#submitButton").click(function() {
-            var dateObj = {
-                startDate: $('#start-date-one').val().trim(),
-                endDate: $('#end-date-one').val().trim(),
-            };
-
-            // Simple date validation: Call the getDate function on the date object created
-            function isValidDate(dateStr) {
-                var d = new Date(dateStr);
-                return !isNaN(d.getDate());
-            }
-
-            if (isValidDate(dateObj.startDate) && isValidDate(dateObj.endDate)) {
-                tableau.connectionData = JSON.stringify(dateObj); // Use this variable to pass data to your getSchema and getData functions
-                tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
-                tableau.submit(); // This sends the connector object to Tableau
-            } else {
-                $('#errorMsg').html("Enter valid dates. For example, 2016-05-08.");
-            }
+            tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
+            tableau.submit(); // This sends the connector object to Tableau
         });
     });
 })();
